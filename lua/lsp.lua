6c7047ec8b8b19074db5d6fc54ff20ac280201f0
Firdaus Yusof <firdausyusof06@gmail.com>
2025-01-03 02:06:24 +0800

    updated vim.fn.sign_define to vim.diagnostic.config to avoid depreciation

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,10 +47,15 @@ vim.diagnostic.config({
 })
 
 local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+local diagnostic_signs = {}
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. typej
+  table.insert(diagnostic_signs, { name = hl, text = icon, texthl = hl, numhl = "" })
+  --vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
+vim.diagnostic.config({
+  signs = diagnostic_signs,
+}
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(args)
