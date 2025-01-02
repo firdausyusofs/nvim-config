4c7662652b1cb1d42ad8a5e38ddf6b0dca31b6b4
Firdaus Yusof <firdausyusof06@gmail.com>
2025-01-03 02:52:25 +0800

    removed mason

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,34 +1,48 @@
-require("mason").setup({
-  ui = {
-    border = "rounded",
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-})
-
-require("mason-lspconfig").setup({
-  ensure_installed = vim.tbl_keys(require("lsp.servers")),
-})
+-- require("mason").setup({
+--   ui = {
+--     border = "rounded",
+--     icons = {
+--       package_installed = "✓",
+--       package_pending = "➜",
+--       package_uninstalled = "✗",
+--     },
+--   },
+-- })
+--
+-- require("mason-lspconfig").setup({
+--   ensure_installed = vim.tbl_keys(require("lsp.servers")),
+-- })
 require("lspconfig.ui.windows").default_options.border = "single"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local mason_lspconfig = require("mason-lspconfig")
+-- local mason_lspconfig = require("mason-lspconfig")
+--
+-- mason_lspconfig.setup_handlers({
+--   function (server_name)
+--     require("lspconfig")[server_name].setup({
+--       capabilities = capabilities,
+--       on_attach = require("lsp.on_attach").on_attach,
+--       settings = require("lsp.servers")[server_name],
+--       filetypes = (require("lsp.servers")[server_name] or {}).filetypes,
+--     })
+--   end,
+-- })
 
-mason_lspconfig.setup_handlers({
-  function (server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-      on_attach = require("lsp.on_attach").on_attach,
-      settings = require("lsp.servers")[server_name],
-      filetypes = (require("lsp.servers")[server_name] or {}).filetypes,
-    })
-  end,
-})
+for server_name, server_config in pairs(require("lsp.servers")) do
+  local lspconfig = require("lspconfig")
+  local on_attach = require("lsp.on_attach").on_attach
+  local settings = server_config
+  local filetypes = server_config.filetypes
+
+  lspconfig[server_name].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = settings,
+    filetypes = filetypes,
+  })
+end
 
 vim.diagnostic.config({
   title = false,
