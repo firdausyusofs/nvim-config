1c3bff26da4c9471a4b7fbc2eded52c44c8830df
Firdaus Yusof <firdausyusof06@gmail.com>
2025-01-20 19:48:03 +0800

    reenable normal highlight override

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -2,5 +2,5 @@ require("rose-pine").setup({
   disable_background = true,
 })
 
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
