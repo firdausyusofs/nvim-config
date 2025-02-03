54ce06186f75be8c653a61aa651e54c307f4ee25
Firdaus Yusof <firdausyusof06@gmail.com>
2025-02-04 06:22:26 +0800

    moved mini.comment config

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -5,10 +5,3 @@ require("mini.surround").setup()
 require("nvim-treesitter.configs").setup({
   enable_autocmd = false,
 })
-require("mini.comment").setup({
-  options = {
-    custom_commentstring = function()
-      return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
-    end,
-  },
-})
