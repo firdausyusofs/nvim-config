2d510bd98ce975a17c204508456ecd23fabdb910
Firdaus Yusof <firdausyusof06@gmail.com>
2025-02-04 05:54:36 +0800

    added mini comment

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -2,3 +2,10 @@ require("mini.pairs").setup()
 require("mini.colors").setup()
 require("mini.ai").setup()
 require("mini.surround").setup()
+require("mini.comment").setup({
+  options = {
+    custom_commentstring = function()
+      return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+    end,
+  },
+})
