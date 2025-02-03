01868cb0a8865aa422d25e0c8f52a235603e09f9
Firdaus Yusof <firdausyusof06@gmail.com>
2025-02-04 06:32:06 +0800

    disable mini

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -1,7 +1,7 @@
-require("mini.pairs").setup()
-require("mini.colors").setup()
-require("mini.ai").setup()
-require("mini.surround").setup()
-require("nvim-treesitter.configs").setup({
-  enable_autocmd = false,
-})
+-- require("mini.pairs").setup()
+-- require("mini.colors").setup()
+-- require("mini.ai").setup()
+-- require("mini.surround").setup()
+-- require("nvim-treesitter.configs").setup({
+--   enable_autocmd = false,
+-- })
