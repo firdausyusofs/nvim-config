67574b03225f974284157483ff18f5cf4c87a6d4
Firdaus Yusof <firdausyusof06@gmail.com>
2025-03-14 18:45:39 +0800

    commented flutter

--- after/plugin/flutter.lua
+++ after/plugin/flutter.lua
@@ -1,5 +1,5 @@
-require("flutter-tools").setup({})
-
-vim.keymap.set("n", "<leader>Fr", "<cmd>FlutterRun<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>FR", "<cmd>FlutterRestart<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>Fq", "<cmd>FlutterQuit<CR>", { noremap = true })
+-- require("flutter-tools").setup({})
+--
+-- vim.keymap.set("n", "<leader>Fr", "<cmd>FlutterRun<CR>", { noremap = true })
+-- vim.keymap.set("n", "<leader>FR", "<cmd>FlutterRestart<CR>", { noremap = true })
+-- vim.keymap.set("n", "<leader>Fq", "<cmd>FlutterQuit<CR>", { noremap = true })
