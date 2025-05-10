8cc930f5754a1917fc68185f670dcf537bfb0cb7
Firdaus Yusof <firdausyusof06@gmail.com>
2025-05-11 05:22:01 +0800

    added rust

---
+++ ftplugin/rust.lua
@@ -0,0 +1,14 @@
+-- set local shiftwidth=4 tabstop=8 softtabstop=4 expandtab:
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = false
+
+if vim.fn.has("nvim") == 1 then
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.rs",
+    callback = function()
+      vim.lsp.buf.format({ timeout_ms = 1000 })
+    end
+  })
+end
