83705469d31f80eb39998188faeab10e075c2429
Firdaus Yusof <firdausyusof06@gmail.com>
2025-05-23 05:06:32 +0800

    updated sourcekit config

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -128,17 +128,17 @@ return {
   },
   ols = {},
   ocamllsp = {},
-  sourcekit = {}
-    -- sourcekit = {
-    --     cmd = {
-    --         "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
-    --     },
-    --     root_dir = function(filename, _)
-    --         local util = require("lspconfig.util")
-    --         return util.root_pattern("buildServer.json")(filename)
-    --             or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-    --             or util.find_git_ancestor(filename)
-    --             or util.root_pattern("Package.swift")(filename)
-    --     end,
-    -- }
+  -- sourcekit = {}
+  sourcekit = {
+    cmd = {
+      "sourcekit-lsp",
+    },
+    root_dir = function(filename, _)
+      local util = require("lspconfig.util")
+      return util.root_pattern("buildServer.json")(filename)
+        or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+        or util.find_git_ancestor(filename)
+        or util.root_pattern("Package.swift")(filename)
+    end,
+  }
 }
