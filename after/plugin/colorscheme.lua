59c9caf387401a959d59abb1b86263490d599c94
Firdaus Yusof <firdausyusof06@gmail.com>
2025-05-29 16:50:53 +0800

    set theme to gruvbox

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,6 +1,9 @@
-require("rose-pine").setup({
-  disable_background = true,
-})
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- require("rose-pine").setup({
+--   disable_background = true,
+-- })
+--
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- set termguicolors
+vim.opt.termguicolors = true
+vim.cmd("colorscheme gruvbox")
