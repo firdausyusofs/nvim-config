55692257477efda04e23fee98b8ac4449cc85f90
Firdaus Yusof <firdausyusof06@gmail.com>
2025-07-05 07:43:04 +0800

    moved harpoon.lua to lua

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,23 +1,23 @@
-local harpoon = require("harpoon")
-harpoon.setup()
-
-vim.keymap.set("n", "<m-h><m-m>", function()
-  harpoon:list():add()
-end)
-vim.keymap.set("n", "<m-h><m-r>", function()
-  harpoon:list():remove()
-end)
-vim.keymap.set("n", "<m-h><m-c>", function()
-  harpoon:list():clear()
-end)
-vim.keymap.set("n", "<m-h><m-l>", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-
--- Set <space>1..<space>5 be my shortcuts to moving to the files
-for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
-  vim.keymap.set("n", string.format("<space>%d", idx), function()
-    harpoon:list():select(idx)
-  end)
-end
-
+-- local harpoon = require("harpoon")
+-- harpoon.setup()
+--
+-- vim.keymap.set("n", "<m-h><m-m>", function()
+--   harpoon:list():add()
+-- end)
+-- vim.keymap.set("n", "<m-h><m-r>", function()
+--   harpoon:list():remove()
+-- end)
+-- vim.keymap.set("n", "<m-h><m-c>", function()
+--   harpoon:list():clear()
+-- end)
+-- vim.keymap.set("n", "<m-h><m-l>", function()
+--   harpoon.ui:toggle_quick_menu(harpoon:list())
+-- end)
+--
+-- -- Set <space>1..<space>5 be my shortcuts to moving to the files
+-- for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+--   vim.keymap.set("n", string.format("<space>%d", idx), function()
+--     harpoon:list():select(idx)
+--   end)
+-- end
+--
