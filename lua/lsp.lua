87989500039b366416ab54349a8ebbe459d394ad
Firdaus Yusof <firdausyusof06@gmail.com>
2025-07-09 09:22:04 +0800

    revert

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -57,14 +57,14 @@ lsp.sourcekit.setup({
   --   -- command = { "sourcekit-lsp/.build/release/sourcekit-lsp" },
   -- }),
   cmd = { "nc", "192.168.0.82", "8081" },
-  root_dir = function(filename, _)
-      local util = require("lspconfig.util")
-      return util.root_pattern("buildServer.json")(filename)
-        or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-        or util.find_git_ancestor(filename)
-        or util.root_pattern("Package.swift")(filename)
-  end,
-  filetypes = { "swift" },
+  -- root_dir = function(filename, _)
+  --     local util = require("lspconfig.util")
+  --     return util.root_pattern("buildServer.json")(filename)
+  --       or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+  --       or util.find_git_ancestor(filename)
+  --       or util.root_pattern("Package.swift")(filename)
+  -- end,
+  -- filetypes = { "swift" },
 })
 
 vim.diagnostic.config({
