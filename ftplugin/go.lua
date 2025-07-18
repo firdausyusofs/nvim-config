e7e0c7bbb05cd016a56b5ee115700abd5fae18ee
Firdaus Yusof <firdausyusof06@gmail.com>
2025-07-19 02:22:18 +0800

    added fold for golang

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -4,6 +4,11 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = false
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldenable = true
+
 if vim.fn.has("nvim") == 1 then
   vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.go",
