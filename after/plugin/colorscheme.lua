4412bb31e5536a15bd193f7bfb5b646a35251926
Firdaus Yusof <firdausyusof06@gmail.com>
2025-07-19 00:43:09 +0800

    change colorscheme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -6,10 +6,12 @@
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- set termguicolors
 
-local color_overrides = require("utils.color_overrides")
+-- local color_overrides = require("utils.color_overrides")
 
 vim.opt.termguicolors = true
 
-color_overrides.setup_colorscheme_overrides()
+-- color_overrides.setup_colorscheme_overrides()
 
-vim.cmd("colorscheme custom")
+-- vim.cmd("colorscheme custom")
+--
+vim.cmd("colorscheme gruvbuddy")
